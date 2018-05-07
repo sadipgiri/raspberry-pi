632e4f45a519872fcd43ca5a959c5bf872acae79
Sadip Giri <sadipgiri@Sadips-Mac-Book-Pro.local>
2018-05-06 22:45:59 -0400

    some changes

--- sensors.py
+++ sensors.py
@@ -14,12 +14,20 @@ time.sleep(0.5)    # sleeping for 0.5 seconds
 # reading data
 # data returns a list of [Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC]
 data = bus.read_i2c_block_data(0x44, 0x00, 6)
- 
-# we just need Temp MSB and Humidity LSB so that we can convert to other temperature scales
-temp = data[0] * 256 + data[1]
+
+temp_msb = data[0]
+temp_lsb = data[1]
+temp_crc = data[2]
+
+humidity_msb = data[3]
+humidity_lsb = data[4]
+humidity_crc = data[5]
+
+# we need Temp MSB, LSB and Humidity LSB, CRC so that we can convert to other temperature scales
+temp = temp_msb * 256 + temp_lsb
 celcius_temp = -45 + (175 * temp / 65535.0)
 fahren_temp = -49 + (315 * temp / 65535.0)
-humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+humidity = 100 * (humidity_msb * 256 + humidity_lsb) / 65535.0
 
 
 if __name__ == '__main__':
